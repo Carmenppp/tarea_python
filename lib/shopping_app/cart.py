8c30c7bdb7821700d3bc1233018dabeecf525233
Carmen Parra <estelaparracarmen@gmail.com>
2024-03-18 12:44:15 -0300

    actualizar

--- lib/shopping_app/cart.py
+++ lib/shopping_app/cart.py
@@ -1,7 +1,7 @@
 
 
 class Cart():
-
+    from ownable import set_owner
     from item_manager import show_items
 
     def __init__(self, owner):
@@ -22,11 +22,19 @@ class Cart():
 
     def check_out(self):
         if self.owner.wallet.balance < self.total_amount():
-            pass    # Eliminar pase al codificar el método check_out.
-         # Requisitos
-         #: el monto de la compra de todos los artículos en el carrito (Cart#items) se transfiere de la billetera del propietario del carrito a la billetera del propietario del artículo.
-         #: la propiedad de todos los artículos del carrito (Cart#items) se transfiere al propietario del carrito.
-         # - El contenido del carrito (Cart#items) está vacío.
+            print("Saldo insuficiente")
+        else:
+            self.owner.wallet.withdraw(self.total_amount())
+            print("Saldo transferido")
+            
+            vendedor = self.items[0].owner
+            vendedor.wallet.deposit(self.total_amount())
+
+            for item in self.items_list():
+                item.set_owner(self.owner)
+            print("Los items ahora tienen como owner al cliente")
+
+            self
          # Consejo
          # - Cartera del propietario del carrito ==> self.owner.wallet
          # - Cartera del propietario del artículo ==> item.owner.wallet
